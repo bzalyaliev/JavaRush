03b16f72d1cd786d28300b8615b6cb68fe77c5ec
Bulat Zalyaliev <bzalyaliev@gmail.com>
2022-01-30 13:23:38 +0300

    Перепись населения

--- 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
@@ -1,31 +1,42 @@
-/*package com.javarush.task.task08.task0815;
+package com.javarush.task.task08.task0815;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
-/* 
+/*
 Перепись населения
 */
 
-/*
 public class Solution {
     public static Map<String, String> createMap() {
-        //напишите тут ваш код
+        Map <String, String> map = new HashMap<>();
+        map.put("Petty", "Tom");
+        map.put("Guilbert", "Paul");
+        map.put("Greenwood", "John");
+        map.put("White", "Jack");
+        map.put("Osbourne", "Ozzy");
+        map.put("Morello", "Tom");
+        map.put("McCartney", "Shawn");
+        map.put("Starr", "Ringo");
+        map.put("West", "Kanye");
+        map.put("Lord", "John");
 
+        return map;
     }
 
     public static int getCountTheSameFirstName(Map<String, String> map, String name) {
-        //напишите тут ваш код
-
+        List<String> firstName = new ArrayList<String>(map.values());
+        return Collections.frequency(firstName, name);
     }
 
     public static int getCountTheSameLastName(Map<String, String> map, String lastName) {
-        //напишите тут ваш код
-
+        List<String> lastNames = new ArrayList<String>(map.keySet());
+        return Collections.frequency(lastNames, lastName);
     }
 
     public static void main(String[] args) {
-
+        System.out.println(createMap());
+        System.out.println(getCountTheSameFirstName(createMap(), "Paul"));
+        System.out.println(getCountTheSameLastName(createMap(), "Lord"));
     }
 }
-*/
